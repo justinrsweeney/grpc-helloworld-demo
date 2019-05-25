dcf770ee672d2160fe698ac530226346532b0cb8
Justin Sweeney <justin.sweeney@zoominfo.com>
2019-05-25 15:52:13 -0600

    Initial commit

---
+++ src/main/java/com/sweeney/grpchelloworld/GrpcHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.sweeney.grpchelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcHelloworldApplication.class, args);
+    }
+
+}
