0c8022dd4b7e233b13aca8eb6b292fe1118f4777
Justin Sweeney <justin.sweeney@zoominfo.com>
2021-10-09 16:33:36 -0600

    Adding exception handling

--- grpc-helloworld-server/src/main/java/com/sweeney/grpchelloworld/service/GreeterService.java
+++ grpc-helloworld-server/src/main/java/com/sweeney/grpchelloworld/service/GreeterService.java
@@ -3,14 +3,26 @@ package com.sweeney.grpchelloworld.service;
 import com.sweeney.grpchelloworld.GreeterGrpc;
 import com.sweeney.grpchelloworld.HelloReply;
 import com.sweeney.grpchelloworld.HelloRequest;
+import com.sweeney.grpchelloworld.InvalidRequestException;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @GRpcService
 public class GreeterService extends GreeterGrpc.GreeterImplBase {
 
+    private static final Logger logger = Logger.getLogger(GreeterService.class.getName());
+
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        if (request.getName().isEmpty()) {
+            logger.log(Level.SEVERE, "No name was provided for request");
+            responseObserver.onError(new InvalidRequestException("A name must be provided for the greeting"));
+        }
+
+        logger.log(Level.INFO, "Greeting: " + request.getName());
         responseObserver.onNext(HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
         responseObserver.onCompleted();
     }
